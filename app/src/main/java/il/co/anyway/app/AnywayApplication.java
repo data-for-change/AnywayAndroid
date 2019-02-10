69cf3e70b4b59c3cf65784602ba465eabda38a60
Lior <liork.dagan@gmail.com>
2019-02-10 13:22:59 +0200

    Added strict mode

--- app/src/main/java/il/co/anyway/app/AnywayApplication.java
+++ app/src/main/java/il/co/anyway/app/AnywayApplication.java
@@ -1,6 +1,7 @@
 package il.co.anyway.app;
 
 import android.app.Application;
+import android.os.StrictMode;
 
 import com.squareup.leakcanary.LeakCanary;
 
@@ -9,6 +10,26 @@ public class AnywayApplication extends Application {
     public void onCreate() {
         super.onCreate();
         initLeakCanary();
+        initStrictMode();
+    }
+
+    private void initStrictMode() {
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()
+                    .penaltyLog()
+                    .penaltyDialog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+        super.onCreate();
     }
 
     private void initLeakCanary() {
@@ -17,4 +38,6 @@ public class AnywayApplication extends Application {
         }
         LeakCanary.install(this);
     }
+
+
 }
