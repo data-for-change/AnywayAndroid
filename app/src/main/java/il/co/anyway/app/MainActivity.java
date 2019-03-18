ba29f6c2162dbc733a609e63e495a9b1c3374d65
Lior <liork.dagan@gmail.com>
2019-03-18 23:26:42 +0200

    Adjust to camera movements

--- app/src/main/java/il/co/anyway/app/MainActivity.java
+++ app/src/main/java/il/co/anyway/app/MainActivity.java
@@ -32,7 +32,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener, GoogleMap.OnCameraIdleListener {
 
     private DrawerLayout mDrawerLayout;
     private GoogleMap mGoogleMap;
@@ -44,7 +44,6 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         prepareToolbar();
-
         FloatingActionButton floatingActionButton = findViewById(R.id.floating_find_my_location_button);
         floatingActionButton.setOnClickListener(this);
 
@@ -53,11 +52,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mapFragment.getMapAsync(this);
     }
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         mMyMarker = mGoogleMap.addMarker(new MarkerOptions().position(new LatLng( 32.071613, 34.795165 )).title("Marker"));
+        mGoogleMap.setOnCameraIdleListener(this);
     }
 
     @Override
@@ -92,58 +91,67 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.floating_find_my_location_button:
-//                Intent intent = new Intent(this, TestNetworkActivity.class);
+                //getMarkersForCurrentView();
+                break;
+        }
+    }
+
+    private void getMarkersForCurrentView() {
+        //                Intent intent = new Intent(this, TestNetworkActivity.class);
 //                startActivity(intent);
 
 //              LatLng currentPoint =  mGoogleMap.getCameraPosition().target;
 //          String zoom = String.valueOf(mGoogleMap.getCameraPosition().zoom);
-                VisibleRegion visibleRegion = mGoogleMap.getProjection().getVisibleRegion();
-                GatDataAPI service = RetrofitNetworkManager.getClient().create(GatDataAPI.class);
+        VisibleRegion visibleRegion = mGoogleMap.getProjection().getVisibleRegion();
+        GatDataAPI service = RetrofitNetworkManager.getClient().create(GatDataAPI.class);
 
-                // http://www.anyway.co.il/markers?ne_lat=32.06828054640345&ne_lng=34.777443297207355&sw_lat=32.06362514879965&sw_lng=34.77358058094978&zoom=17&thin_markers=false&start_date=1451599200&end_date=1501534800&format=json&show_markers=1&show_discussions=1&show_urban=3&show_intersection=3&show_lane=3&show_day=7&show_severe=1&show_fatal=1&show_light=1&show_inaccurate=1&start_date=01%2F01%2F2016&end_date=01%2F08%2F2017
-                //https://www.anyway.co.il/markers?ne_lat=32.074933503710426&ne_lng=34.76937219500542&sw_lat=32.06562343302103&sw_lng=34.7616470977664&zoom=17&thin_markers=false&start_date=1451599200&end_date=1501534800&format=json&show_markers=1&show_discussions=1
-                String ne_lat = "32.074933503710426";
-                String ne_lng = "34.76937219500542";
-                String sw_lat = "32.06562343302103";
-                String sw_lng = "34.7616470977664";
-                if (visibleRegion!= null && visibleRegion.latLngBounds != null &&
-                        visibleRegion.latLngBounds.northeast != null && visibleRegion.latLngBounds.southwest != null) {
-                    ne_lat = String.valueOf(visibleRegion.latLngBounds.northeast.latitude);
-                    ne_lng = String.valueOf(visibleRegion.latLngBounds.northeast.longitude);
-                    sw_lat = String.valueOf(visibleRegion.latLngBounds.southwest.latitude);
-                    sw_lng = String.valueOf(visibleRegion.latLngBounds.southwest.longitude);
-                }
-                String zoom = "17";
-                String thin_markers = "false";
-                String start_date = "1451599200";
-                String end_date = "1501534800";
-                String format = "json";
-                String show_markers = "1";
-                String show_discussions = "1";
+        // http://www.anyway.co.il/markers?ne_lat=32.06828054640345&ne_lng=34.777443297207355&sw_lat=32.06362514879965&sw_lng=34.77358058094978&zoom=17&thin_markers=false&start_date=1451599200&end_date=1501534800&format=json&show_markers=1&show_discussions=1&show_urban=3&show_intersection=3&show_lane=3&show_day=7&show_severe=1&show_fatal=1&show_light=1&show_inaccurate=1&start_date=01%2F01%2F2016&end_date=01%2F08%2F2017
+        //https://www.anyway.co.il/markers?ne_lat=32.074933503710426&ne_lng=34.76937219500542&sw_lat=32.06562343302103&sw_lng=34.7616470977664&zoom=17&thin_markers=false&start_date=1451599200&end_date=1501534800&format=json&show_markers=1&show_discussions=1
+        String ne_lat = "32.074933503710426";
+        String ne_lng = "34.76937219500542";
+        String sw_lat = "32.06562343302103";
+        String sw_lng = "34.7616470977664";
+        if (visibleRegion!= null && visibleRegion.latLngBounds != null &&
+                visibleRegion.latLngBounds.northeast != null && visibleRegion.latLngBounds.southwest != null) {
+            ne_lat = String.valueOf(visibleRegion.latLngBounds.northeast.latitude);
+            ne_lng = String.valueOf(visibleRegion.latLngBounds.northeast.longitude);
+            sw_lat = String.valueOf(visibleRegion.latLngBounds.southwest.latitude);
+            sw_lng = String.valueOf(visibleRegion.latLngBounds.southwest.longitude);
+        }
+        String zoom = "17";
+        String thin_markers = "false";
+        String start_date = "1451599200";
+        String end_date = "1501534800";
+        String format = "json";
+        String show_markers = "1";
+        String show_discussions = "1";
 
-                Call<Markers> call = service.getAllMarkers(ne_lat, ne_lng, sw_lat, sw_lng, zoom, thin_markers, start_date, end_date, format, show_markers, show_discussions);
+        Call<Markers> call = service.getAllMarkers(ne_lat, ne_lng, sw_lat, sw_lng, zoom, thin_markers, start_date, end_date, format, show_markers, show_discussions);
 //                mProgressBar.setVisibility(View.VISIBLE);
-                call.enqueue(new Callback<Markers>() {
+        call.enqueue(new Callback<Markers>() {
 
-                    @Override
-                    public void onResponse(Call<Markers> call, Response<Markers> response) {
-                        String responseBody = response.body().toString();
+            @Override
+            public void onResponse(Call<Markers> call, Response<Markers> response) {
+                String responseBody = response.body().toString();
 //                        mProgressBar.setVisibility(View.GONE);
 //                        generateDataList(response.body().getMarkers());
-                        mMarkerList = response.body().getMarkers();
-                        for (MapMarker marker : mMarkerList){
-                            String severity = marker.getAccident_severity() == null ? "":marker.getAccident_severity().toString();
+                mMarkerList = response.body().getMarkers();
+                for (MapMarker marker : mMarkerList){
+                    String severity = marker.getAccident_severity() == null ? "":marker.getAccident_severity().toString();
                             mGoogleMap.addMarker(new MarkerOptions().position(new LatLng(marker.getLatitude(), marker.getLongitude())).title(severity));
                         }
                     }
 
                     @Override
-                    public void onFailure(Call<Markers> call, Throwable t) {
+            public void onFailure(Call<Markers> call, Throwable t) {
 //                        mProgressBar.setVisibility(View.GONE);
-                        Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
-                    }
-                });
-                break;
-        }
+                Toast.makeText(getApplicationContext(), "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public void onCameraIdle() {
+        getMarkersForCurrentView();
     }
 }
